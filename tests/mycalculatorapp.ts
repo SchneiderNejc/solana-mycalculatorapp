7baaf4e8bfd8adefba0d1566719fbde4620018bc
Nejc Schneider <nejc.sch@gmail.com>
2024-03-24 23:30:26 +0100

    set provider

--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -2,6 +2,8 @@ const assert = require('assert')
 const anchor = require('@project-serum/anchor')
 const {SystemProgram} = anchor.web3
 describe('mycalculatordapp', () => {
+    const provider = anchor.Provider.local();
+    anchor.setProvider(provider)
     const calculator = anchor.web3.Keypair.generated()
     const program = anchor.workspace.Mycalculatordapp
 
