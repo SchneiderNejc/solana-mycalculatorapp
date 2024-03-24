fd5e3bc1a540b728f565f8dd7936b64f931d60f8
Nejc Schneider <nejc.sch@gmail.com>
2024-03-24 23:30:17 +0100

    declare instances

--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -2,5 +2,7 @@ const assert = require('assert')
 const anchor = require('@project-serum/anchor')
 const {SystemProgram} = anchor.web3
 describe('mycalculatordapp', () => {
+    const calculator = anchor.web3.Keypair.generated()
+    const program = anchor.workspace.Mycalculatordapp
 
 })
