5068733c112c6e81ca9d2d31c873475fbb022fa2
Nejc Schneider <nejc.sch@gmail.com>
2024-03-25 23:52:14 +0100

    fix keypair.generate

--- tests/mycalculatorapp.ts
+++ tests/mycalculatorapp.ts
@@ -3,10 +3,10 @@ const anchor = require('@project-serum/anchor')
 const {SystemProgram} = anchor.web3
 describe('mycalculatordapp', () => {
     anchor.setProvider(provider)
-    const calculator = anchor.web3.Keypair.generated()
     const program = anchor.workspace.Mycalculatordapp
     const provider = anchor.getProvider();
 
     it('Creates a caluclator', async() => {
     })
+    const calculator = anchor.web3.Keypair.generate();
 })
