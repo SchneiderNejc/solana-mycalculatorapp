1948db1d16c72d9309781d156d291066eb1cc505
Nejc Schneider <nejc.sch@gmail.com>
2024-03-27 14:02:38 +0100

    add multiply()

--- programs/mycalculatorapp/src/lib.rs
+++ programs/mycalculatorapp/src/lib.rs
@@ -24,6 +24,12 @@ pub mod mycalculatorapp {
         calculator.result = num1 - num2;
         Ok(())
     }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
